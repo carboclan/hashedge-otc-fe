68d3a30b798124d8fcc8340ff380388b2f494c41
Tal Rasha <talrasha007@gmail.com>
2019-04-15 16:30:24 +0800

    api server dev config.

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.API_SERVER || 'http://localhost:3000',
         changeOrigin: true
       }
     },
