433be793d7992f625de61c5d2eca17304ce5822c
Tal Rasha <talrasha007@gmail.com>
2019-04-24 23:39:09 +0800

    swap721 whitelist.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -9,6 +9,14 @@
       <button v-on:click="mint(erc20.address, erc20.mintAddress, 1000000)">MINT1,000,000</button>
     </li>
   </ul>
+  <p>Swap721 Tokens</p>
+  <ul>
+    <li v-for="erc721 of swap721Tokens" :key="erc721.address">
+      <label>{{erc721.name}}</label>
+      <input placeholder="address" size="40" v-model="erc721.whitelistAddress" />
+      <button v-on:click="addWhitelisted(erc721.address, erc721.whitelistAddress)">Add to whitelist.</button>
+    </li>
+  </ul>
 </div>
 </template>
 
@@ -16,6 +24,7 @@
 import { hashedgeContracts } from '../web3';
 
 const erc20Tokens = [];
+const swap721Tokens = [];
 
 export default {
   name: 'Test',
@@ -28,16 +37,29 @@ export default {
           name: await hashedgeContracts.erc20Tokens[address].name()
         });
       }
+
+      const addresses721 = Object.keys(hashedgeContracts.swap721Tokens);
+      for (const address of addresses721) {
+        swap721Tokens.push({
+          address,
+          name: await hashedgeContracts.swap721Tokens[address].name()
+        });
+      }
     }
   },
   methods: {
     mint(tokenAddr, mintAddr, value) {
       hashedgeContracts.erc20Tokens[tokenAddr].mint(mintAddr, value * 10 ** 18);
+    },
+
+    addWhitelisted(tokenAddr, whitelistAddr) {
+      hashedgeContracts.swap721Tokens[tokenAddr].addWhitelisted(whitelistAddr);
     }
   },
   data() {
     return {
-      erc20Tokens
+      erc20Tokens,
+      swap721Tokens
     };
   }
 }
